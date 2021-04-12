922da69c19b36d3ce715504e85cb2b9edd427916
Abhijeetsingh0 <67339987+Abhijeetsingh0@users.noreply.github.com>
2021-04-12 16:18:58 +0530

    few changes

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -7,18 +7,8 @@ export default function About(){
 
         <div className='px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full whiteClo'>
 
-        Hello,  I am Anshika Rao,  and I can help you to plan your garden! 
-        I have been practicing gardening and organically food growing since 3 years or more.  
-        Growing pure food and plants is the desire of my soul, 
-        after learning to grow everything from local farmers , 
-        now I am headed towards learning it professionally by pursuing hons. in agriculture from Bhopal.  
+        Hey, I am Abhijeet Singh and this is my blogs, The purpose of this site to share my experience about things I do.  
         <p>
-        I did my first year with Bsc computer science but now I changed my stream to Agriculture.  
-        I grow indoor pollution control plants,  
-        and also some vegetables and food crops.  
-        U can buy some vegetable seedlings right at my home. 
-        Contact me in my instagram account at
-        <h4><i><a href="https://www.instagram.com/green_lambent/">green_lambent</a></i></h4> and email me <h4><i>shobha00rani@gmail.com</i></h4>    
 
         </p>
         
--- src/componenets/Post.js
+++ src/componenets/Post.js
@@ -29,7 +29,7 @@ function Post() {
        <main className="bg-green-100 min-h-screen p-12 postBg">
            <section className="container mx-auto">
                <h1 className="text-5xl flex justify-center cursive "> Welcome to blog page :) </h1>
-               <h2 className="text-lg flex justify-center text-gray-800 mb-12">Hope you find my blogs helpful ! </h2>
+               <h2 className="text-lg flex justify-center text-gray-800 mb-12">Hope you find somthing interesting! </h2>
                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
 
                 {postData && postData.map((post, index)=>(
