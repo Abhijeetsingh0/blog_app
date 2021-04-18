a34c4cd8e4ee508634326cbf48784edbe75f157c
Abhijeetsingh0 <67339987+Abhijeetsingh0@users.noreply.github.com>
2021-04-18 19:30:34 +0530

    fixing bottom_bar

--- src/componenets/BottomBar.js
+++ src/componenets/BottomBar.js
@@ -4,15 +4,9 @@ function Project() {
     return (
       <main>
          <div className="footer">
-            <h2 className="whiteClo ml-20 textSize">Contact me:- </h2>
-         <div className="grid-container">
-            <div className="whiteClo">Gmail:-</div>
-            <div className="whiteClo">anish16253481@gmail.com</div>
-         </div>
-         <div className="grid-container">
-            <div className="whiteClo">Discord:-</div>
-            <div className="whiteClo">Hisoka#4210</div>
-         </div>
+            <h2 className="whiteClo ml-10 textSize">Contact me:- </h2>
+            <h1 class="text-center" style={{fontSize:'20px'}}>Gmail-anish16253481@gmail.com</h1>
+            <h1 class="text-center" style={{fontSize:'20px'}}>Discord-Hisoka#4210</h1>
          </div>
       </main>
     )
